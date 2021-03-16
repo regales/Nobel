a8524b07c1ba8992bff2b543e1173252b2494a93
ryanthedev432 <66663195+ryanthedev432@users.noreply.github.com>
2021-03-16 17:08:01 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,12 @@
 module.exports = (client) => {
   console.log('Im alive as ' + client.user.tag)
   setInterval(function () {
-      // bot activity
-      client.user.setActivity('*help | codes', { type: 'LISTENING' })
+      
+    client.user.setActivity('*help | codes', { type: 'LISTENING' })
 
-      client.user.setActivity('Live Music',{
-        type: 'STREAMING',
-        url: 'https://www.twitch.tv/chillhopmusic'
-      })
-    }, 2000)
+    client.user.setActivity("Live Music", {
+      type: "STREAMING",
+      url: "https://www.twitch.tv/chillhopmusic"
+    });
+  }, 5000)
 }

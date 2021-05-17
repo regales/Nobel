822abf5f5075c835ea268a4b89f4bec231f71f11
ryanthedev432 <66663195+ryanthedev432@users.noreply.github.com>
2021-05-17 14:50:18 +0800

    run err

--- commands/slap.js
+++ commands/slap.js
@@ -5,7 +5,7 @@ const sendError = require("../util/error")
 module.exports = {
     name: "slap",
     aliases: [" "],
-    execute(client, message, args) {
+    run: async(client, message, args) => {
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if(!args[0]) return sendError(`<:xmark:314349398824058880> Try run again the command but next time please specify a user!`);
         if(user.id === message.author.id) return sendError(`Cheer up! Don't slap yourself.`);

2b0261573dc4f4214cb0de0600361682aceb6e45
ryanthedev432 <66663195+ryanthedev432@users.noreply.github.com>
2021-05-17 14:59:56 +0800

    err

--- commands/laugh.js
+++ commands/laugh.js
@@ -7,8 +7,8 @@ module.exports = {
     aliases: ["hehe"],
     run: async(client, message, args) => {
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        if(!args[0]) return sendError(`<:xmark:314349398824058880> Try run again the command but next time please specify a user!`);
-        if(user.id === message.author.id) return sendError(`Cheer up! Don't laugh on yourself.`);
+        if(!args[0]) return sendError(`<:xmark:314349398824058880> Try run again the command but next time please specify a user!` , message.channel);
+        if(user.id === message.author.id) return sendError(`Cheer up! Don't laugh on yourself.` , message.channel);
         const jsonLaugh = fs.readFileSync(
           '../roleplay.json'
           );
--- commands/slap.js
+++ commands/slap.js
@@ -7,8 +7,8 @@ module.exports = {
     aliases: [" "],
     run: async(client, message, args) => {
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        if(!args[0]) return sendError(`<:xmark:314349398824058880> Try run again the command but next time please specify a user!`);
-        if(user.id === message.author.id) return sendError(`Cheer up! Don't slap yourself.`);
+        if(!args[0]) return sendError(`<:xmark:314349398824058880> Try run again the command but next time please specify a user!` , message.channel);
+        if(user.id === message.author.id) return sendError(`Cheer up! Don't slap yourself.` , message.channel);
         const jsonSlap = fs.readFileSync(
           '../roleplay.json'
           );

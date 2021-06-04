57253d0ba91af936b3b0d49285b10133507842b9
ryanthedev432 <66663195+ryanthedev432@users.noreply.github.com>
2021-06-04 17:51:36 +0800

    ✨✨ [Code]

--- commands/fun/tictactoe.js
+++ commands/fun/tictactoe.js
@@ -1,14 +1,20 @@
-const { tictactoe } = require('reconlx')
+const { TicTacToe } = require('weky');
 
 module.exports = {
     name : 'tictactoe',
     run: async(client, message, args) => {
-        const member = message.mentions.members.first() 
-            if(!member)  return  message.channel.send('<:xmark:848019597907329085> Please specify a member')
-        
-        new tictactoe({
-            player_two: member, 
-            message: message
+        const opponent = message.mentions.users.first();
+        if (!opponent) return message.channel.send(`<:xmark:848019597907329085> **Please mention who you want to challenge at tictactoe.**`);
+
+        const game = new TicTacToe({
+            message: message,
+            opponent: opponent, //opponent
+            xColor: 'red', //x's color
+            oColor: 'blurple', //zero's color
+            xEmoji: '❌',  //the x emoji
+            oEmoji: '0️⃣' ,//the zero emoji
         })
+        game.start()//start da game
+
     }
 }

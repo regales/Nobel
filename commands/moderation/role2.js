44a0cba2866ca407d7e109adda87d3c9a992570d
ryanthedev432 <66663195+ryanthedev432@users.noreply.github.com>
2021-06-13 00:04:50 +0900

    ✨✨ [Code]

--- commands/moderation/role2.js
+++ commands/moderation/role2.js
@@ -13,7 +13,7 @@ module.exports = {
         const embed = new MessageEmbed()
            
             
-            .setDescription(`\n**__Reaction Role For Games__**\n<:9233_CSGO_SN:853283905633648650> **-** <@&852402918735872030>\n<:3670phoenixthinkaboutit:853286124854968320> **-** <@&852403011250815026>\n<:AmongUs_POG:774200272410247200> **-** <@&852403027499679745>\n<a:3048_Pepe_Sword:846728664069701632> **-** <@&851005635393749032>`)
+            .setDescription(`\n**__Reaction Role For Games__**\n<:9233_CSGO_SN:853283905633648650> **-** <@&852402918735872030>\n<:3670phoenixthinkaboutit:853286124854968320> **-** <@&852403011250815026>\n<:amongus:770726914415263754> **-** <@&852403027499679745>\n<a:3048_Pepe_Sword:846728664069701632> **-** <@&851005635393749032>`)
             
             
             .setColor('BLACK')

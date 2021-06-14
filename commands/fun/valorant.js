0a3c234a02874cd464770e2616c583e06a239e06
ryanthedev432 <66663195+ryanthedev432@users.noreply.github.com>
2021-06-14 09:59:45 +0900

    ✨✨ [Code]

--- commands/fun/valorant.js
+++ commands/fun/valorant.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js')
 const Discord = require('discord.js')
 module.exports = {
     name : 'valorant',
-    aliases: [valo , agent , valorant ],
+    aliases: ['valo' , 'agent' , 'valorant' ],
     
 
 

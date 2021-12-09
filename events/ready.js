f244f1ea4955364e2ba5ebf36fa82dbbe065480e
Regales <66663195+regales@users.noreply.github.com>
2021-12-09 15:43:53 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,11 @@
 module.exports = (client) => {
   console.log(`${client.user.username} ✅`) 
   const activities = [
-    `@Nobel *help`,
-    `@Nobel *help`,
-    `In ${client.guilds.cache.size} Servers`,
-    `Support Server ====> discord.gg/ZJevrUQ46Q`
+    `*help`,
   ];
   
   let i = 0;
-  setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`, { type: 'PLAYING' }), 20000);
+  setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`, { type: 'LISTENING' }), 20000);
   
 
 }
